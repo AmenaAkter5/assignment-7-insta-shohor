56f956b0f1a435788b149501adc71d0193c68ef3
unknown <latapata1952@gmail.com>
2022-03-11 00:13:24 +0600

    Bug-3 fixed

--- js/app.js
+++ js/app.js
@@ -30,7 +30,9 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  // console.log(text);
+  // 3. BUG is Here
+  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
